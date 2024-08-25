f26b3f05b5aa5d7df9c0289fe8887f1944e546a9
Debdip Bhattacharya <dbhattacharya1912@gmail.com>
2024-08-26 00:54:52 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -6,21 +6,23 @@ import Cart from './pages/Cart';
 import Collection from './pages/Collection';
 import Contact from "./pages/Contact"
 import Login from './pages/Login';
-import Orders from "./pages/Orders"
 import PlaceOrder from './pages/PlaceOrder';
 import Product from './pages/Product';
+import Orders from './pages/Orders';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <NavBar/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/cart' element={<Cart/>}/>
-      <Route path='/collection' element={<Collection/>}/>
+      <Route path='/collections' element={<Collection/>}/>
       <Route path='/contact' element={<Contact/>}/>
       <Route path='/login' element={<Login/>}/>
-      <Route path='/orders' element={<Orders/>}/>
+      <Route path='/login' element={<Orders/>}/>
       <Route path='/placeorder' element={<PlaceOrder/>}/>
       <Route path='/product/:productId' element={<Product/>}/>
     </Routes>
