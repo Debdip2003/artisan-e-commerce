dba0d9a1719d5db987f14f2a3f55a0b7798a4c6b
Debdip Bhattacharya <dbhattacharya1912@gmail.com>
2024-08-29 22:56:46 +0530

    collection page done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,26 +15,28 @@ const Footer = () => {
           </p>
         </div>
         <div>
-            <p className="text-xl font-medium mb-5">COMPANY</p>
-            <ul className="flex flex-col gap-1 text-gray-600">
-                <li>Home</li>
-                <li>About us</li>
-                <li>Delivery</li>
-                <li>Privacy Policy</li>
-            </ul>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
         </div>
 
         <div>
-            <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
-            <ul className="flex flex-col gap-1 text-gray-600">
-                <li>+91 9007245362</li>
-                <li>forever@gmail.com</li>
-            </ul>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+91 9007245362</li>
+            <li>forever@gmail.com</li>
+          </ul>
         </div>
       </div>
       <div>
-        <hr/>
-        <p className="py-5 text-sm text-center">Copyright 2024@ forever.com - All Right Reserved</p>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2024@ forever.com - All Right Reserved
+        </p>
       </div>
     </div>
   );
